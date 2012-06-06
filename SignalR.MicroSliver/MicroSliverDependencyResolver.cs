da7bf902931fded8e73aa3a6187b8268bb8dc19e
Jeremiah Billmann <jbillmann@gmail.com>
2012-06-05 23:54:49 -0500

    Initial commit of MicroSliver SignalR DI Resolver

---
+++ SignalR.MicroSliver/MicroSliverDependencyResolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using MicroSliver;
+
+namespace SignalR.MicroSliver {
+    public class MicroSliverDependencyResolver : DefaultDependencyResolver{
+
+        private readonly IIoC _ioc;
+        public MicroSliverDependencyResolver(IIoC ioc) 
+        {
+            if (ioc == null)
+            {
+                throw new ArgumentNullException("ioc");
+            }
+            _ioc = ioc;
+        }
+
+        public override object GetService(Type serviceType) 
+        {
+            return _ioc.GetByType(serviceType);
+        }
+
+        public override IEnumerable<object> GetServices(Type serviceType) 
+        {
+            yield return _ioc.GetByType(serviceType);
+        }
+    }
+}
---
+++ SignalR.MicroSliver/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SignalR.MicroSliver")]
+[assembly: AssemblyDescription("A MicroSliver dependency resolver implementation for SignalR.")]
+[assembly: AssemblyCompany("Jeremiah Billmann")]
+[assembly: AssemblyCopyright("Copyright © Jeremiah Billmann 2012")]
+
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyInformationalVersion("0.5.0.0")]
