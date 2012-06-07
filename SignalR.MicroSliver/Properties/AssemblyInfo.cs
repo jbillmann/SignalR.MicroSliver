48d666d2cf51c4254fada90d0d5a69cfc05a9a18
Jeremiah Billmann <jbillmann@gmail.com>
2012-06-06 19:19:06 -0500

    Updated version

--- SignalR.MicroSliver/Properties/AssemblyInfo.cs
+++ SignalR.MicroSliver/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SignalR.MicroSliver")]
-[assembly: AssemblyDescription("A MicroSliver dependency resolver implementation for SignalR.")]
+[assembly: AssemblyDescription("A MicroSliver dependency resolver implementation and hub activator for SignalR.")]
 [assembly: AssemblyCompany("Jeremiah Billmann")]
 [assembly: AssemblyCopyright("Copyright © Jeremiah Billmann 2012")]
 
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.0.0")]
-[assembly: AssemblyInformationalVersion("0.5.0.0")]
+[assembly: AssemblyVersion("0.5.1.0")]
+[assembly: AssemblyFileVersion("0.5.1.0")]
+[assembly: AssemblyInformationalVersion("0.5.1.0")]
